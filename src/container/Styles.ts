c48d8f7f19caa1adb2b428893255ed0748b6e8ca
Heder Alves <alvesheder92@gmail.com>
2024-03-19 00:53:29 -0300

    merge: dev to master (#15)
    * fix: update the node to fix version incompatibility
    
    * fix: add the required node versions to the github build
    
    * fix: node version
    
    * fix: remove obsolete versions
    
    * fix: update the node to fix version incompatibility
    
    * fix: update the node to fix version incompatibility
    
    * fix: update the node to fix version incompatibility
    
    * fix: fix: update the node to fix version incompatibility
    
    * fix: yml file from ci/cd github
    
    * fix: yml file from ci/cd github
    
    * docs: improve the readme
    
    * Feat: create component container
    
    * remove: thing stories
    
    * feat: create the container component
    
    * feat: create styles for the container component
    
    * feat: create story for container component
    
    * feat: add imports and exports of the container component
    
    * feat: add the box stories component

---
+++ src/container/Styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { ContainerProps } from "./Container";
+
+export const ContainerStyles = styled.div<ContainerProps>`
+    display: flex;
+    justify-content: ${props => props.justify || "flex-start"};
+    gap: ${props => (props.gap ? `${props.gap}px` : "0")};
+`;
