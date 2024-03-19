fbcdc164670d7f6422a498881997851414f24c70
Heder Alves <alvesheder92@gmail.com>
2024-03-18 23:54:51 -0300

    feat: create the container component

---
+++ src/container/Container.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { ContainerStyles } from "./Styles";
+
+export interface ContainerProps {
+    gap?: number;
+    justify?: "flex-start" | "flex-end" | "center" | "space-between" | "space-around" | "space-evenly";
+    children: React.ReactNode;
+}
+
+export const Container = (props: ContainerProps) => {
+    return (
+        <ContainerStyles justify={props.justify} gap={props.gap}>
+            {props.children}
+        </ContainerStyles>
+    );
+}
